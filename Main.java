d7fcf8fc00d2680e1bf3804fe06538e5b0746e84
Hach <sosisochka08009@gmail.com>
2023-12-19 11:56:28 +0300

    up4

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Launcher nedelnoi davnosti");
@@ -34,27 +35,30 @@ public class Main {
             public void actionPerformed(ActionEvent e) {
                 String inputText = textField.getText();
                 if (inputText.equals("lazzyz") || inputText.equals("excellent")) {
-                    JFrame newFrame = new JFrame("Launcher - Vzlom jopi");
-                    newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                    newFrame.setSize(720, 450);
-                    newFrame.add(new JPanel() {
+                    JFrame Frame2 = new JFrame("Launcher - Vzlom jopi");
+                    Frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                    Frame2.setSize(720, 450);
+                    Frame2.add(new JPanel() {
                         @Override
                         protected void paintComponent(Graphics g) {
                             super.paintComponent(g);
                             Image image = new ImageIcon("C:\\Users\\User\\Desktop\\CheatPack\\DKcUpJgAtIc.jpg").getImage();
                             g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
                             g.setColor(Color.WHITE); // Устанавливаем цвет шрифта
-                            g.drawString("[+} Добавлен хуй под твоей мамашей", 10, 50);// Отображаем строку "да да" в координатах (50, 50)
-                            g.drawString("[+} Добавлен хуй под твоей мамашей", 10, 60);
-                            g.drawString("[+} Добавлен хуй под твоей мамашей", 10, 70);
-                            g.drawString("[+} Добавлен хуй под твоей мамашей", 10, 80);
-                            g.drawString("[+} Добавлен хуй под твоей мамашей", 10, 90);
-                            g.drawString("[+} Добавлен хуй под твоей мамашей", 10, 100);
-                            g.drawString("[+} Добавлен хуй под твоей мамашей", 10, 110);
+                            g.drawString("[ + ] Добавлен хуй под твоей мамашей", 10, 10);// Отображаем строку "да да" в координатах (50, 50)
+                            g.drawString("[ + ] Добавлен хуй под твоей мамашей", 10, 20);
+                            g.drawString("[ + ] Добавлен хуй под твоей мамашей", 10, 30);
+                            g.drawString("[ + ] Добавлен хуй под твоей мамашей", 10, 40);
+                            g.drawString("[ + ] Добавлен хуй под твоей мамашей", 10, 50);
+                            g.drawString("[ + ] Добавлен хуй под твоей мамашей", 10, 60);
+                            g.drawString("[ + ] Добавлен хуй под твоей мамашей", 10, 70);
+
+
+
                         }
                     });
 
-                    newFrame.setVisible(true);
+                    Frame2.setVisible(true);
                     frame.dispose(); // Закрываем текущее окно
                 } else {
                     JOptionPane.showMessageDialog(frame, "net ne plavelno idi naxyi");
@@ -66,6 +70,7 @@ public class Main {
         panel.add(textField);
         panel.add(button);
 
+        frame.add(button);
         frame.add(panel);
         frame.pack();
         frame.setVisible(true);
